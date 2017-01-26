7947882849f7ffa01c8bb5a1e4878fabe12636b3
Johannes Mueller <github@johannes-mueller.org>
2017-01-26 20:51:36 +0100

    Slight code rearrangement for the sake of readability

--- src/cli.rs
+++ src/cli.rs
@@ -10,22 +10,24 @@ pub struct Interface {
 
 impl Interface {
         pub fn new(evt_queue: event::Queue) -> Interface {
-                let jh = thread::spawn( move | | {
-                        loop {
-                                let mut cmd = String::new();
-                                let l = io::stdin().read_line(&mut cmd);
-                                match cmd.chars().next() {
-                                        Some('q') => {
-                                                evt_queue.pass_event(Box::new(QuitEvent));
-                                        }
-                                        _ => {}
-                                }
-                        }
-                });
-
+                let jh = thread::spawn( move | | event_loop(evt_queue) );
                 Interface { join_handle: jh }
         }
 }
+
+pub fn event_loop(evt_queue: event::Queue) {
+        loop {
+                let mut cmd = String::new();
+                let l = io::stdin().read_line(&mut cmd);
+                match cmd.chars().next() {
+                        Some('q') => {
+                                evt_queue.pass_event(Box::new(QuitEvent));
+                        }
+                        _ => {}
+                }
+        }
+}
+
 
 pub struct QuitEvent;
 
